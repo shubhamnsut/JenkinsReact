dbecc3120a73828298353e4b89fa1d0396060772
Shubham <shubhamiit111@gmail.com>
2024-03-09 11:20:57 +0530

    first-jenkins-code-added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './NavBar';
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Navbar= () =>{
+  return (
+  <div>
+    <li>
+     Dogs
+    </li>
+    <li>
+      Cats
+    </li>
+   
+  </div>
+  );
+}
+export default Navbar;
